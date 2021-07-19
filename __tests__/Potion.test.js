3c8c9bf6470128190c4b7397dbee4dd95d4515fd
Yussef Rafat <yussefrafat5908@gmail.com>
2021-07-19 16:12:37 -0400

    potion test complete

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@jest/globals');
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+});
