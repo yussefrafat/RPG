376d30f42d28af8a4974e2d753ed61bd2b2f5b18
Yussef Rafat <yussefrafat5908@gmail.com>
2021-07-21 13:44:57 -0400

    player object

---
+++ __tests__/Player.test.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@jest/globals');
+const { exception } = require('console');
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion')
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+      expect.arrayContaining([expect.any(Object)])
+    );
+  });
